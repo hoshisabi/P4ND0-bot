b89c466fca0b74ad5c69bf7b452b41bafceaf968
hoshisabi <chapmand@gmail.com>
2022-08-29 06:11:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import feedparser
 
 client = discord.Client()
 
-#def get_quote():
+def get_quote():
 #  response = requests.get("https://zenquotes.io/api/random")
 #  json_data = json.loads(response.text)
 #  quote = json_data[0]['q'] + " -" + json_data[0]['a']
