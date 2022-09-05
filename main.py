a01675dfa2c011463187bd204f15a06c29964f53
Daniel E. Chapman II <chapmand@gmail.com>
2022-09-04 23:58:46 -0400

    minor tweak:

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import feedparser
 client = discord.Client()
 
 def get_quote():
-#  response = requests.get("https://zenquotes.io/api/random")
-#  json_data = json.loads(response.text)
-#  quote = json_data[0]['q'] + " -" + json_data[0]['a']
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " -" + json_data[0]['a']
   quote = "Requests not working, just giving static quote --Render"
   return(quote)
 
