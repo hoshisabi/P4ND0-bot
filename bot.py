206450535c4e11683b6ca5a4676022c95e86ead5
Daniel E. Chapman II <chapmand@gmail.com>
2022-09-05 06:29:21 -0400

    tweaks to bot over time

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
-import discord
-from discord.ext import commands
-import random
-import os
-import requests
 import json
-import feedparser
+import random
+import typing
+from datetime import datetime
 
-intents = discord.Intents.default()
-intents.members = True
-intents.message_content = True
+import discord
+import feedparser
+import markdownify
+import requests
+from discord.ext import commands
+
+
+def to_discordtimestamp(incoming_time):
+    return int(datetime.fromisoformat(incoming_time).timestamp())
 
 
 description = '''
@@ -22,43 +25,50 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', description=description, intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('------')
 
+
+@bot.command()
+async def schedule(ctx, arg: typing.Optional[bool]):
+    """Pulls the most recent schedule of upcoming events from Warhorn displayed in local time
+    Pass "True" to get full details of event. """
+    await rss(ctx)
+
+
 @bot.command()
 async def quote(ctx):
-    """Generate a random quote"""
+    """Generate a random quote (no parameters)"""
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
-    quote = "Quote courtesy of https://zenquotes.io/\n" + json_data[0]['q'] + "\n\t-" + json_data[0]['a'] + "\n"
+    quote = f"{json_data[0]['q']}\n\t-*{json_data[0]['a']}*\n"
     print(quote)
-    await ctx.send(quote)
-
-@bot.command()
-async def quote2(ctx):
-    """Generate a random quote: experimental"""
-    response = requests.get("https://zenquotes.io/api/random")
-    json_data = json.loads(response.text)
-    embed = discord.embed(title="Quote", color = discord.Color.blue())
-    quote = "Quote courtesy of https://zenquotes.io/\n" + json_data[0]['q'] + "\n\t-" + json_data[0]['a'] + "\n"
-    print(quote)
-    embed.add_field(field = json_data[0]['q'], name = json_data[0]['a'], inline = false)
-    await ctx.send(embed)
-
+    embed = discord.Embed(title="Quote", color=discord.Color.blue())
+    embed.description = quote
+    embed.set_author(name="zenquotes.io", url="https://zenquotes.io/")
+    await ctx.send(embed=embed)
 
 
 @bot.command()
-async def rss(ctx):
-    """Get the schedule of upcoming events off of the Warhorn"""
-    rss = feedparser.parse("https://warhorn.net/events/pandodnd/schedule/Ya7RynA9U_XsaE_Ve6Ht.atom")
-    print(rss.entries[0])
-    rsslist = [f"{x.title} {x.gd_when['starttime']}: {x.link}" for x in rss.entries]
-    rssoutput = "\n\t".join(rsslist)
-    reply = f"The following games are coming up:\n\n\t{rssoutput}\n"
-    print(reply)
-    await ctx.send(reply)
+async def rss(ctx, arg: typing.Optional[bool]):
+    """Pulls the most recent schedule of upcoming events from Warhorn displayed in local time
+    Pass "True" to get full details of event. """
+    print(arg)
+    rss_feed = feedparser.parse("https://warhorn.net/events/pandodnd/schedule/Ya7RynA9U_XsaE_Ve6Ht.atom")
+    print(rss_feed)
+    desc_text = f"The following games are upcoming on this server, click on a link to schedule a seat.\n"
+    for x in rss_feed.entries:
+        desc_text += f"  * [{x.title}]({x.link}) <t:{to_discordtimestamp(x.gd_when['starttime'])}>"
+        if (arg): desc_text += f"\n>{markdownify.markdownify(x.summary)}"
+        desc_text += "\n"
+    embed = discord.Embed(title="Schedule", type="rich", description=desc_text, color=discord.Color.green())
+
+    print(f"Printed out schedule:\n{desc_text}")
+    await ctx.send(embed=embed)
+
 
 @bot.command()
 async def roll(ctx, dice: str):
@@ -68,8 +78,13 @@ async def roll(ctx, dice: str):
     except Exception:
         await ctx.send('Format has to be in NdN!')
         return
+    result = ', '.join(str(random.randint(1, limit)) for _ in range(rolls))
+    embed = discord.Embed(title="Dice", type="rich", description=result, color=discord.Color.blue())
 
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await ctx.send(result)
+    print(f"Dice result: {result}")
+    await ctx.send(embed=embed)
 
-bot.run(os.getenv('TOKEN'))
+with open("env.txt") as fd:
+    token = fd.readline()
+
+bot.run(token)
--- rss.py
+++ rss.py
@@ -1,13 +1,23 @@
 import feedparser
-import json
+from datetime import datetime
 
+
+def to_discordtimestamp(str):
+    x = int(datetime.fromisoformat(str).timestamp())
+    return x
+
+print("2022-09-07T19:00:00-04:00")
+print(to_discordtimestamp("2022-09-07T19:00:00-04:00"))
+print("---")
 
 rss = feedparser.parse("https://warhorn.net/events/pandodnd/schedule/Ya7RynA9U_XsaE_Ve6Ht.atom")
 print(rss.entries[0])
 print(rss.entries[0].gd_when)
-print(rss.entries[0].gd_when['starttime'])
+strtime = to_discordtimestamp(rss.entries[0].gd_when['starttime'])
+print(f"-{strtime}-")
+#print(rss.entries[0].summary)
 
-rsslist = [f"{x.title} {x.gd_when['starttime']}: {x.link}" for x in rss.entries]
+rsslist = [f"* [{x.title}]({x.link}) <t:{to_discordtimestamp(x.gd_when['starttime'])}>" for x in rss.entries]
 rssoutput = "\n\t".join(rsslist)
 reply = f"The following games are coming up:\n\n\t{rssoutput}\n"
 print(reply)
